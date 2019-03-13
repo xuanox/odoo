9bc208f4b9f8c7c36775399e6b36db61d0a4e308
Luis Dominguez <ldominguez@hermecsolutions.com>
2019-03-13 16:57:27 -0500

    Agregado hs_invoice_fiscal

---
+++ hs_invoice_fiscal/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+	'name': "hs_invoice_fiscal",
+
+	'summary': """
+		Facturacion en impresora fiscal """,
+
+	'description': """
+		Permite la conexion de facturas con impresoras fiscales.
+		Requiere la aplicacion urano corriendo dentro de la computadora cliente
+	""",
+
+	'author': "HS Consulting S.A.",
+	'website': "http://www.hconsul.com/odoo/",
+
+	# Categories can be used to filter modules in modules listing
+	# Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+	# for the full list
+	'category': 'report',
+	'version': '1.06',
+
+	# any module necessary for this one to work correctly
+	'depends': ['base', 'account'],
+
+	# always loaded
+	'data': [
+		# 'security/ir.model.access.csv',
+		'views/invoice_view.xml',
+		'views/respartner_view.xml',
+	],
+	# only loaded in demonstration mode
+	'demo': [
+		'demo/demo.xml',
+	],
+}
---
+++ hs_invoice_fiscal/controllers/controllers.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from werkzeug import exceptions, url_decode
+from werkzeug.datastructures import Headers
+from werkzeug.test import Client
+from werkzeug.wrappers import BaseResponse
+
+from odoo.http import Controller, route, request
+from odoo.tools import html_escape
+from odoo.addons.web.controllers.main import _serialize_exception, content_disposition
+from odoo.tools.safe_eval import safe_eval
+
+import base64
+
+class Binary(Controller):
+	@route('/report/text', type='http', auth="public")
+	def download_document(self,model,field,id,filename=None, **kw):
+		Model = request.env[model]
+		#invoice_number = Model.browse(int(id)).number
+		invoice_number = id
+		filecontent = base64.b64decode(Model.get_file_content(int(id)))
+		if not filecontent:
+			return request.not_found()
+		else:
+			if not filename:
+				#filename = '%s.%s' % (invoice_number,'txt')
+				filename = Model.get_file_name(int(id))
+				return request.make_response(filecontent,
+						[('Content-Type', 'text/plain'),
+						('Content-Disposition', content_disposition(filename))])
