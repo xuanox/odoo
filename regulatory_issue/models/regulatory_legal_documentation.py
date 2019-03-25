8b00734358f187d49ecb356a0eef75327e087c2f
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-03-24 19:28:59 -0500

    update app regulatory

--- regulatory_issue/models/regulatory_legal_documentation.py
+++ regulatory_issue/models/regulatory_legal_documentation.py
@@ -27,10 +27,41 @@ from odoo.tools.translate import _
 from odoo.exceptions import UserError, ValidationError
 
 
+class RegulatoryLegalDocumentationStage(models.Model):
+    _name = 'regulatory.legal.documentation.stage'
+    _description = 'Regulatory Legal Documentation Stage'
+    _order = 'sequence, id'
+
+    name = fields.Char('Name', required=True, translate=True)
+    sequence = fields.Integer('Sequence', default=20)
+    fold = fields.Boolean('Folded in Regulatory Legal Documentation Pipe')
+    done = fields.Boolean('Request Done')
+
+
+class RegulatoryLegalDocumentationType(models.Model):
+    _name = 'regulatory.legal.documentation.type'
+    _description = ' Regulatory Legal Documentation Type'
+    _order = 'id'
+
+    name = fields.Char(string="Type", required=True, translate=True)
+
+
 class RegulatoryLegalDocumentation(models.Model):
     _name = 'regulatory.legal.documentation'
+    _description = 'Regulatory Legal Documentation'
+    _order = 'id'
+
+
+    @api.returns('self')
+    def _default_stage(self):
+        return self.env['regulatory.legal.documentation.stage'].search([], limit=1)
 
     name = fields.Char(string="Legal Document", required=True, translate=True)
-    code=fields.Char('Code')
+    apostille_number=fields.Char('Apostille Number')
+    certificate_number=fields.Char('Certificate Number')
+    expedition_date = fields.Date(u'Expedition Date')
     expiration_date = fields.Date(u'Expiration Date')
     description=fields.Text('Description')
+    observation=fields.Text('Observation')
+    type_id = fields.Many2one('regulatory.legal.documentation.type', u'Type')
+    stage_id = fields.Many2one('regulatory.legal.documentation.stage', string='Stage', default=_default_stage)
