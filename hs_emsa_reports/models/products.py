215f59ebc06217a0ff7619a71b067bbb93698b32
Luis Dominguez <ldominguez@hermecsolutions.com>
2019-04-04 10:53:15 -0500

    Actualizado campos
    Se realizaron actualizaciones menores al modulo, se eliminaron las relaciones a factura y producto

--- hs_emsa_reports/models/products.py
+++ hs_emsa_reports/models/products.py
@@ -20,9 +20,11 @@ class EmsaAccountDetail(models.AbstractModel):
 class ProductsReport(models.Model):
 	_inherit = "account.invoice.line"
 
-	hs_date_invoice = fields.Date(related='invoice_id.date_invoice', string="Fecha Factura", store=False)
-	hs_type_invoice = fields.Selection(related='invoice_id.type', string="Categoria Factura", store=False)
-	hs_product_price = fields.Float(related='product_id.standard_price', string="Precio Bodega", store=False)
+	hs_invoice_date = fields.Date(related='invoice_id.date_invoice', string="Fecha Factura", store=False)
+	hs_invoice_number = fields.Char(related='invoice_id.number', string="Documento", store=False)
+	hs_invoice_type = fields.Selection(related='invoice_id.type', string="Categoria Factura", store=False)
+	hs_product_price = fields.Float(related='product_id.standard_price', string="Precio Bodega", store=True)
+	hs_product_name = fields.Char(related='product_id.name', string="Precio Bodega", store=False)
 	hs_product_categ = fields.Char(related='product_id.categ_id.name', string="Categoria Producto", store=False)
 
 
