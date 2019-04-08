c101f51f185d3c945a5e51b3fb1fc36ee23d1ff5
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-04-07 21:35:44 -0500

    update

--- regulatory_issue/__manifest__.py
+++ regulatory_issue/__manifest__.py
@@ -23,7 +23,7 @@
 {
     'name': 'Regulatory Issue Management',
     'summary': """Regulatory Issue Management""",
-    'version': '12.0.1.1.7',
+    'version': '12.0.1.1.8',
     'author': 'Rocendo Tejada',
     'website': "http://www.electronicamedica.com",
     'company': 'Electrónica Médica',
