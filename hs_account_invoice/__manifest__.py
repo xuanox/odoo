0d2aded8706c5cdaaed65c066248694338d8bcd1
Luis Dominguez <luis.dominguez19@yahoo.es>
2019-05-09 09:54:14 -0500

    update 3
    Corregido el valor numnber a number

--- hs_account_invoice/__manifest__.py
+++ hs_account_invoice/__manifest__.py
@@ -17,7 +17,7 @@
 	# Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
 	# for the full list
 	'category': 'other',
-	'version': '0.2',
+	'version': '0.3',
 
 	# any module necessary for this one to work correctly
 	'depends': ['base', 'account'],
@@ -30,6 +30,6 @@
 
 	# only loaded in demonstration mode
 	'demo': [
-		'demo/demo.xml',
+		#'demo/demo.xml',
 	],
 }
