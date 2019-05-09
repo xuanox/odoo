9530aa557bb25cbd3dbc204afcee6aebeb497aa6
Luis Dominguez <luis.dominguez19@yahoo.es>
2019-05-09 11:52:03 -0500

    vendor bill report 03

--- hs_emsa_reports/models/vendor_bills.py
+++ hs_emsa_reports/models/vendor_bills.py
@@ -1,2 +1,14 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
+
+
+class VendorBillsReport(models.Model):
+	_inherit = "account.invoice"
+
+
+	@api.multi
+	def get_report(self):
+		"""
+		Call when button 'Get Report' clicked.
+		"""
+		return self.env.ref('hs_emsa_reports.report_vendor_bill').report_action(self)
