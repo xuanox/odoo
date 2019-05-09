87c8dd8f6bf28d1c3ce9134a554c9c7393aa52e3
Luis Dominguez <luis.dominguez19@yahoo.es>
2019-05-09 09:40:24 -0500

    module hs_account_invoice
    se agrego el modulo hs_account_invoice para modificar el modulo account_invoice

---
+++ hs_account_invoice/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+	'name': "hs_account_invoice",
+
+	'summary': """
+		Modificaciones a facturacion """,
+
+	'description': """
+		Realiza modificaciones al sistema de facturacion en la empresa EMSA.
+		- Agrega la columna 'Tipo de Factura' a Invoice/Credit Note
+	""",
+
+	'author': "HS Consulting S.A.",
+	'website': "http://www.hconsul.com/odoo/",
+
+	# Categories can be used to filter modules in modules listing
+	# Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+	# for the full list
+	'category': 'other',
+	'version': '0.1',
+
+	# any module necessary for this one to work correctly
+	'depends': ['base', 'account'],
+
+	# always loaded
+	'data': [
+		# 'security/ir.model.access.csv',
+	],
+	# only loaded in demonstration mode
+	'demo': [
+		'demo/demo.xml',
+	],
+}
