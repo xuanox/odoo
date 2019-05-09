2c7d4d22a8e71738d05e6bfaf5c2afcc8f92e8a6
Luis Dominguez <luis.dominguez19@yahoo.es>
2019-05-09 14:58:03 -0500

    update 52

--- hs_emsa_reports/__manifest__.py
+++ hs_emsa_reports/__manifest__.py
@@ -17,7 +17,7 @@
 	# for the full list
 	'category': 'Invoicing &amp; Payments',
 	'license': 'LGPL-3',
-	'version': '0.51',
+	'version': '0.52',
 	
 	# any module necessary for this one to work correctly
 	'depends': ['base', 'account', 'account_reports'],
--- hs_emsa_reports/models/vendor_bills.py
+++ hs_emsa_reports/models/vendor_bills.py
@@ -7,24 +7,24 @@ class VendorBillsReport(models.AbstractModel):
 
 	@api.model
 	def _get_report_values(self, docids, data=None):
+		report = self.env["ir.actions.report"]._get_report_from_name('hs_emsa_reports.vendor_bill_template')
 		amount = 0.00
-		docs = []
+		lines = []
 
 		for invoice in self:
 			amount += invoice.amount_total
-			docs.append({
+			lines.append({
 				'number': invoice.number,
 				'date': invoice.due_date,
 				'amount': invoice.amount_total
 			})
 
-
-		report = self.env["ir.actions.report"]._get_report_from_name('hs_emsa_reports.vendor_bill_template')
 		return {
 			'doc_ids': docids,
 			'doc_model': report.model,
 			'amount': amount,
-			'docs': docs,
+			'columns': lines,
+			'docs': self.env[report.model].browse(docids),
             'report_type': data.get('report_type') if data else '',
 		}
 
