d75529396553fea1a54c315a95a16a23193a2ba8
Luis Dominguez <luis.dominguez19@yahoo.es>
2019-05-10 17:09:16 -0500

    update 65

--- hs_emsa_reports/models/vendor_bills.py
+++ hs_emsa_reports/models/vendor_bills.py
@@ -6,11 +6,28 @@ import datetime
 class VendorBillsReport(models.AbstractModel):
 	_name = "report.hs_emsa_reports.vendor_bill_template"
 
+
+	def get_date_invoice(self, invoice_datetime):
+		"""
+		Obtenemos la Fecha en que fue creada la nota credito dentro del Odoo
+		y luego le aplicamos la diferencia horaria para obtener la hora UTC de
+		America - Bogota
+		"""
+		if type(invoice_datetime) == str:
+			date_invoice = datetime.strptime(invoice_datetime, 
+							'%Y-%m-%d').strftime('%d/%m/%Y') or ''
+			return date_invoice
+		else:
+			date_invoice = invoice_datetime.strftime('%d/%m/%Y') or ''
+			return date_invoice
+
+
 	@api.model
 	def _get_report_values(self, docids, data=None):
-		report = self.env["ir.actions.report"]._get_report_from_name('hs_emsa_reports.vendor_bill_template')
+		report_name = 'hs_emsa_reports.vendor_bill_template'
+		report = self.env["ir.actions.report"]._get_report_from_name(report_name)
 		amount = 0.00
-		current_date = (datetime.date.today()).strftime("%d/%m/%Y")
+		current_date = self.get_date_invoice(datetime.date.today())
 		lines = []
 		document = docids
 		if len(docids) > 1:
@@ -21,7 +38,8 @@ class VendorBillsReport(models.AbstractModel):
 			amount += item.amount_total
 			lines.append({
 				'number': item.number,
-				'date': item.date_invoice,
+				'reference': item.reference,
+				'date': self.get_date_invoice(item.date_invoice),
 				'amount': item.amount_total
 			})
 
