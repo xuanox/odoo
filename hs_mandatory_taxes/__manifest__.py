37866151fe3f4dccc35d551a00f502db6d5c5d4c
Luis Dominguez <luis.dominguez19@yahoo.es>
2019-05-15 16:45:22 -0500

    taxes update 05

--- hs_mandatory_taxes/__manifest__.py
+++ hs_mandatory_taxes/__manifest__.py
@@ -16,7 +16,7 @@
 	# Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
 	# for the full list
 	'category': 'Sales',
-	'version': '0.4',
+	'version': '0.5',
 
 	# any module necessary for this one to work correctly
 	'depends': ['base', 'account', 'purchase', 'sale'],
