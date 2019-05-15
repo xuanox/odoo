8e8cf552e077adaba5b822b0386d2ca764980ca7
Luis Dominguez <luis.dominguez19@yahoo.es>
2019-05-15 14:08:01 -0500

    stock update 3

--- hs_stock_extra/models/stock.py
+++ hs_stock_extra/models/stock.py
@@ -3,12 +3,12 @@ from odoo import api, fields, models
 
 class StockPickingInherit(models.Model):
 	_inherit = "stock.picking"
-	hs_category = fields.Char(related='picking_type_id.name', string="Operation Type", store=False)
-	hs_state = fields.Selection(_compute_picking_type, store=False)
+	#hs_category = fields.Char(related='picking_type_id.name', string="Operation Type", store=False)
+	hs_state = fields.Selection(selection='_get_selection_content', store=False)
 
 
 	@api.depends('picking_type_id')
-	def _compute_picking_type(self):
+	def _get_selection_content(self):
 		if self.picking_type_id.name == "Delivery Orders":
 			return [
 					('draft', 'Draft'),
