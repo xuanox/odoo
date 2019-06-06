7a0cb50b2c5d122f5f51d8695f09283278039b91
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-06-06 16:55:45 -0500

    up

--- helpdesk_technical_support/wizard/__init__.py
+++ helpdesk_technical_support/wizard/__init__.py
@@ -5,6 +5,7 @@
 #    Copyright (C) 2013-2018 CodUP (<http://codup.com>).
 #
 ##############################################################################
+
 from . import assign_ticket
 from . import pending
 from . import cause
