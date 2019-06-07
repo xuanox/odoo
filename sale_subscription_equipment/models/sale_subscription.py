1faad5e09ad7f990f7478eb9c34a722833740026
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-06-06 22:05:54 -0500

    up

--- sale_subscription_equipment/models/sale_subscription.py
+++ sale_subscription_equipment/models/sale_subscription.py
@@ -5,10 +5,29 @@ class SaleSubscription(models.Model):
     _name = "sale.subscription"
     _inherit = "sale.subscription"
 
+    equipments_lines = fields.One2many('sale.subscription.equipment', 'analytic_account_id', 'Equipments', copy=True, readonly=True, states={'draft': [('readonly', False)]})
 
 class SaleSubscriptionEquipment(models.Model):
     _name = 'sale.subscription.equipment'
     _description = 'Sale Subscription Equipment'
+
+    equipment_id = fields.Many2one('equipment.equipment', string='Equipment', required=True)
+    analytic_account_id = fields.Many2one('sale.subscription', string='Subscription')
+    name = fields.Text('Description', index=True, required=True)
+
+    @api.onchange('equipment_id')
+    def onchange_equipment_id(self):
+        """ On change of product it sets product quantity, tax account, name,
+        uom of product, unit price and price subtotal. """
+        if not self.equipment_id:
+            return
+
+        partner = self.analytic_account_id.partner_id
+        if partner and self.equipment_id:
+            self.name = self.equipment_id.display_name
+
+class SaleSubscriptionTemplate(models.Model):
+    _inherit = "sale.subscription.template"
 
     equipment_id = fields.Many2one('equipment.equipment', string='Equipment', required=True)
     analytic_account_id = fields.Many2one('sale.subscription', string='Subscription')
