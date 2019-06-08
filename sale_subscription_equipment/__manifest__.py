9a3eb485e35585676f6c2bbb8aadab1521f89dfd
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-06-08 12:58:55 -0500

    Update __manifest__.py

--- sale_subscription_equipment/__manifest__.py
+++ sale_subscription_equipment/__manifest__.py
@@ -7,7 +7,7 @@
     'description': """
 This module allows you to set a deferred revenue on your subscriptions.
 """,
-    'depends': ['sale_subscription', 'equipment', 'helpdesk'],
+    'depends': ['sale_subscription', 'equipment', 'helpdesk', 'helpdesk_technical_support'],
     'data': [
         'views/sale_subscription_views.xml',
         'views/equipment_views.xml',
