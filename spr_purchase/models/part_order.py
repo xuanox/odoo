e037401dacb2d12be396027590fc00296d99fad6
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-06-09 17:27:17 -0500

    Update part_order.py

--- spr_purchase/models/part_order.py
+++ spr_purchase/models/part_order.py
@@ -50,25 +50,6 @@ class Part(models.Model):
         return 0
 
     @api.multi
-    def action_part_confirm(self):
-        """ part order state is set to 'To be invoiced' when invoice method
-        is 'Before Part' else state becomes 'Confirmed'.
-        @param *arg: Arguments
-        @return: True
-        """
-        if self.filtered(lambda part: part.state != 'quotation'):
-            raise UserError(_("Only draft Parts can be confirmed."))
-        before_part = self.filtered(lambda part: part.invoice_method == 'b4repair')
-        before_part.write({'state': '2binvoiced'})
-        to_confirm = self - before_part
-        to_confirm_operations = to_confirm.mapped('operations')
-        to_confirm_operations.write({'state': 'confirmed'})
-        #to_confirm.action_part_done()
-        to_confirm.action_confirm()
-        to_confirm.write({'state': 'confirmed'})
-        return True
-
-    @api.multi
     def action_cancel(self):
         result = super(Part, self).action_cancel()
         # When a sale person cancel a SO, he might not have the rights to write
