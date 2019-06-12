3619b064ab134082b48f82feca65e8d30dd496f1
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-06-12 14:57:21 -0500

    up

---
+++ helpdesk_technical_support/models/res_partner.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class Partner(models.Model):
+
+    _inherit = 'res.partner'
+
+    email_pm = fields.Char()
+    email_cm = fields.Char()
