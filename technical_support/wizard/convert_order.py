d0a3ed39d691bb86866ca0de9b91a41b7ed3a8e7
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-06-13 11:52:33 -0500

    u

--- technical_support/wizard/convert_order.py
+++ technical_support/wizard/convert_order.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo
-#    Copyright (C) 2015-2018 emsa (<http://www.electronicamedica.com>).
+#    Copyright (C) 2015-2018 CodUP (<http://codup.com>).
 #
 ##############################################################################
 
@@ -25,14 +25,14 @@ class technical_support_convert_order(models.TransientModel):
                     'parts_uom': line.parts_uom.id,
                     }])
             category_id = 1
-            if order.equipment_id.category_ids:
-                for category in order.equipment_id.category_ids:
+            if order.asset_id.category_ids:
+                for category in order.asset_id.category_ids:
                     category_id = category.id
                     break
             values = {
                 'name': order.description,
                 'category_id': category_id,
-                'maintenance_type': order.maintenance_type if order.maintenance_type != 'pm' else 'cm',
+                'maintenance_type': order.maintenance_type if order.maintenance_type != 'bm' else 'cm',
                 'parts_lines': new_parts_lines,
                 'tools_description': order.tools_description,
                 'labor_description': order.labor_description,
--- technical_support/wizard/reject.py
+++ technical_support/wizard/reject.py
@@ -2,9 +2,23 @@
 ##############################################################################
 #
 #    Odoo
-#    Copyright (C) 2013-2018 emsa (<http://www.electronicamedica.com>).
+#    Copyright (C) 2013-2019 CodUP (<http://codup.com>).
 #
 ##############################################################################
 
 from odoo import api, fields, models
 from odoo import netsvc
+
+class technical_support_request_reject(models.TransientModel):
+    _name = 'technical_support.request.reject'
+    _description = 'Reject Request'
+
+    reject_reason = fields.Text('Reject Reason', required=True)
+
+    def reject_request(self):
+        active_id = self._context.get('active_id')
+        if active_id:
+            request = self.env['technical_support.request'].browse(self._context.get('active_id'))
+            request.write({'reject_reason':self.reject_reason})
+            request.action_reject()
+        return {'type': 'ir.actions.act_window_close',}
