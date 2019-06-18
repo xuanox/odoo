28018cb11f87cc0cfff001f8e33728f4c85567c4
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-06-18 14:54:44 -0500

    Update helpdesk.py

--- helpdesk_technical_support/models/helpdesk.py
+++ helpdesk_technical_support/models/helpdesk.py
@@ -97,6 +97,11 @@ class HelpdeskTicket(models.Model):
         self.write({'stage_id': 3})
         return True
 
+    def update_equipment_state_operative(self):
+        for order in self:
+            if order.equipment_id: order.equipment_id.write({'maintenance_state_id': 21})
+        return True
+
 
 class HelpdeskTeam(models.Model):
     _inherit = 'helpdesk.team'
