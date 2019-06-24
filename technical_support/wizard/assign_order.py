757cf14bbd864319e8534560b6b34f2881d88ef2
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-06-23 22:19:10 -0500

    update

---
+++ technical_support/wizard/assign_order.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo
+#    Copyright (C) 2013-2019 CodUP (<http://codup.com>).
+#
+##############################################################################
+import time
+from odoo import api, fields, models
+from odoo import netsvc
+
+class TechnicalSupportAssign(models.TransientModel):
+    _name = 'technical_support.request.assign'
+    _description = 'Assign Order'
+
+    user_id = fields.Many2one('res.users', string='Assigned to', required=True, track_visibility='onchange', default=lambda self: self.env.user.id, domain=lambda self: [('groups_id', 'in', self.env.ref('helpdesk.group_helpdesk_user').id)])
+    date_planned = fields.Datetime('Planned Date', required=True, default=time.strftime('%Y-%m-%d %H:%M:%S'), track_visibility='onchange')
+    detail = fields.Text('Detail', required=True)
+
+    def assign_order(self):
+        active_id = self._context.get('active_id')
+        if active_id:
+            order = self.env['technical_support.request'].browse(self._context.get('active_id'))
+            order.write({'user_id': self.user_id.id})
+            order.write({'date_planned':self.date_planned})
+            order.write({'detail_new_order':self.detail})
+            order.action_confirm()
+        return {'type': 'ir.actions.act_window_close',}
