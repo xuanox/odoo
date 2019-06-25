6c2529000dc1c920d3bb8f67b9368f4452215b71
Luis Dominguez <luis.dominguez19@yahoo.es>
2019-06-25 17:36:46 -0500

    Update __manifest__.py

--- hs_invoice_fiscal/__manifest__.py
+++ hs_invoice_fiscal/__manifest__.py
@@ -17,7 +17,7 @@
 	# Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
 	# for the full list
 	'category': 'report',
-	'version': '2.4',
+	'version': '2.5',
 
 	# any module necessary for this one to work correctly
 	'depends': ['base', 'account'],
@@ -32,4 +32,7 @@
 	'demo': [
 		'demo/demo.xml',
 	],
+
+	'installable': True,
+	'auto_install': True,
 }
