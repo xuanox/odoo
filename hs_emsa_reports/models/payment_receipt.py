48a03675150fffb8f18bbd69195aef1117300071
Luis Dominguez <luis.dominguez19@yahoo.es>
2019-06-27 09:27:07 -0500

    Add payment_receipt
    Se empezo el desarrollo del reporte de pago de caja

---
+++ hs_emsa_reports/models/payment_receipt.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+from decimal import Decimal
+import datetime
+from hs_emsa_reports.models.numero_letras import Numero_Letra
+
+
+class VendorBillsReport(models.AbstractModel):
+	_name = "report.hs_emsa_reports.payment_receipt_template"
+	_description = 'Payment Receipt Report'
+
+	def get_date_document(self, document_datetime):
+		"""
+		Obtenemos la Fecha en que fue creada la nota credito dentro del Odoo
+		y luego le aplicamos la diferencia horaria para obtener la hora UTC de
+		America - Bogota
+		"""
+		if type(document_datetime) == str:
+			temp_date = datetime.strptime(document_datetime,
+							'%Y-%m-%d').strftime('%d/%m/%Y') or ''
+			return temp_date
+		else:
+			temp_date = document_datetime.strftime('%d/%m/%Y') or ''
+			return temp_date
+
+	"""
+	@api.model
+	def _get_report_values(self, docids, data=None):
+		doc_ids = data['ids']
+		medioPago = data['form']['MedioPago']
+	
+
+	"""
+	@api.model
+	def _get_report_values(self, docids, data=None):
+		report_name = 'hs_emsa_reports.vendor_bill_template'
+		current_date = self.get_date_document(datetime.date.today())
+		conversor = Numero_Letra()
+		docs = self.env["account.payment"].browse(docids)
+		letter_amount = ""
+		amount = ""
+		partner = ""
+		for value in docids:
+			record = self.env["account.payment"].search([('id', '=', value)])
+			letter_amount = record.amount
+			amount = record.amount
+			partner = record.partner_id.name
+
+		return {
+			'doc_ids': docids,
+			'doc_model': "account.payment",
+			"letter_amount": letter_amount,
+			"number_amount": amount,
+			'partner': partner,
+			'docs': docs,
+		}
+
+
+class PaymentReceiptWizard(models.TransientModel):
+	_name = 'payment.receipt.wizard'
+	_description = 'Payment Receipt Wizzard'
+	categoria = fields.Selection(string="Medio de Pago", selection={ 
+									"Efectivo": "Efectivo",
+									"Cheque": "Cheque No.",
+									"Banco": "Banco",
+									"ACH": "ACH",
+									"Pago Tarjeta": "PagoTarjeta"})
+
+
+	@api.multi
+	def get_report(self):
+		doc_ids=self._context.get('active_ids')
+		content = {
+			'ids': doc_ids,
+			'model': "account.payment",
+			'form': {
+				'medioPago': self.categoria,
+			}
+		}
+
+		return self.env.ref('hs_emsa_reports.payment_receipt_template').report_action(self, data=content)
