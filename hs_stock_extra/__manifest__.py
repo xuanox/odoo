3f53ee1a36ffeb0477eda1c2862ef4af22bac610
Luis Dominguez <luis.dominguez19@yahoo.es>
2019-06-27 09:00:49 -0500

    hs_stock update
    Se actualizo el modulo hs_stock_extra para soporte de la columna vendedor

--- hs_stock_extra/__manifest__.py
+++ hs_stock_extra/__manifest__.py
@@ -17,7 +17,7 @@
 	# Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
 	# for the full list
 	'category': 'other',
-	'version': '0.4',
+	'version': '0.5',
 
 	# any module necessary for this one to work correctly
 	'depends': ['base', 'stock'],
