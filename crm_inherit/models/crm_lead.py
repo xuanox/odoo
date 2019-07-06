d63124a9f501df98d2729b1d976c3164096b1d33
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-07-06 17:36:59 -0500

    update

--- crm_inherit/models/crm_lead.py
+++ crm_inherit/models/crm_lead.py
@@ -10,3 +10,19 @@ class Lead(models.Model):
     modality = fields.Selection([('high', 'High Tech'), ('low', 'Low Tech')], string='Modality')
     model_id=fields.Many2one('equipment.model', string='Model')
     date_deadline = fields.Date(required=True)
+
+class CrmLeadCategory(models.Model):
+    _name = "crm.lead.category"
+    _description = "Sales Category"
+
+    name=fields.Char("Category", required=True)
+    description=fields.Text('Description')
+
+class CrmLeadChecklistDemo(models.Model):
+    _name = "crm.lead.checklist.demo"
+    _description = "Checklist Demo"
+    _order = 'sequence'
+
+    name=fields.Char("Item", required=True)
+    sequence=fields.Integer('Sequence')
+    description=fields.Text('Description')
