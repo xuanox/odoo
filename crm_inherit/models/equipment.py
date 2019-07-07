d88654bfbb5d6db8f837c386f994f7805770a4c9
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-07-06 19:34:14 -0500

    update

--- crm_inherit/models/equipment.py
+++ crm_inherit/models/equipment.py
@@ -13,3 +13,4 @@ class EquipmentModality(models.Model):
     _inherit = 'equipment.modality'
 
     category_id=fields.Many2one('crm.lead.category', string='Sales Category')
+    applicationist_id = fields.Many2one('res.users', 'Applicationist', track_visibility='onchange', default=lambda self: self._uid)
