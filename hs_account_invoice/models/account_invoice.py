5c5f7c32b8f7baae26766399f3142b55936c8985
Luis <luis.dominguez19@yahoo.es>
2019-07-17 15:29:38 -0500

    update hs_account_invoice

---
+++ hs_account_invoice/models/account_invoice.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class AccountInvoiceInherit2(models.Model):
+
+	_inherit = "account.invoice"
+
+	hs_doctype_item = fields.Char("Tipo", compute="_compute_doctype_item", stored=False)
+	
+	
+	@api.one
+	@api.depends('type')
+	def _compute_doctype_item(self):
+		if self.type == "out_invoice":
+			self.hs_doctype_item = "F/."
+		elif self.type == "out_refund":
+			self.hs_doctype_item = "N/C "
+		else:
+			self.hs_doctype_item = ""
