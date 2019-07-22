f140366e26a5854ced22aa9c3f383d555ff64229
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-07-22 15:11:52 -0500

    Update crm_lead.py

--- crm_inherit/models/crm_lead.py
+++ crm_inherit/models/crm_lead.py
@@ -26,8 +26,26 @@ class CrmLeadChecklistDemo(models.Model):
     _order = 'sequence'
 
     name=fields.Char("Item", required=True)
-    sequence=fields.Integer('Sequence')
-    description=fields.Text('Description')
+    opportunity_id=fields.Many2one('crm.lead', string='Opportunity', domain="[('type', '=', 'opportunity')]")
+    partner_id = fields.Many2one(
+        'res.partner', 'Contact',
+        index=True,
+        help='Choose partner for whom the order will be invoiced and delivered. You can find a partner by its Name, TIN, Email or Internal Reference.')
+    address_id = fields.Many2one(
+        'res.partner', 'Delivery Address',
+        domain="[('parent_id','=',partner_id)]")
+    default_address_id = fields.Many2one('res.partner', compute='_compute_default_address_id')
+    access_path= fields.Text('Ruta de Acceso')
+    date_planned = fields.Datetime('Delivery Time', required=True, readonly=True, states={'draft':[('readonly',False)]}, default=time.strftime('%Y-%m-%d %H:%M:%S'), track_visibility='onchange')
+    retirement_date = fields.Datetime('Retirement Date', required=True, readonly=True, states={'draft':[('readonly',False)]}, default=time.strftime('%Y-%m-%d %H:%M:%S'), track_visibility='onchange')
+    special_delivery_procedure=fields.Text('Description')
+    note=fields.Text('Description')
+
+    @api.one
+    @api.depends('partner_id')
+    def _compute_default_address_id(self):
+        if self.partner_id:
+            self.default_address_id = self.partner_id.address_get(['contact'])['contact']
 
 class Team(models.Model):
     _inherit = 'crm.team'
