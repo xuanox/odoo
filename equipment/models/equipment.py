cef974c602aea87599ba75a306114301429d7214
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-08-03 21:48:02 -0500

    Update equipment.py

--- equipment/models/equipment.py
+++ equipment/models/equipment.py
@@ -232,7 +232,7 @@ class equipment_equipment(models.Model):
     def name_get(self):
         result = []
         for equipment in self:
-            result.append((equipment.serial, "%s (#%d)" % (equipment.name, equipment.serial)))
+            result.append((equipment.id, "%s (#%d)" % (equipment.name, equipment.id, equipment.serial)))
         return result
 
 class EquipmentBrand(models.Model):
