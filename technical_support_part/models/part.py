d8b0e257f794a3bab682f4822a24e791a7e4c4a5
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-08-07 09:26:58 -0500

    add module Technical Support and Part

---
+++ technical_support_part/models/part.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, fields, models, SUPERUSER_ID, _, exceptions
+import time
+import datetime as dt
+import time, datetime
+from datetime import date
+from datetime import datetime
+from dateutil import relativedelta
+from datetime import datetime, timedelta
+from odoo.exceptions import UserError
+from dateutil.relativedelta import *
+
+
+class Part(models.Model):
+    _inherit = 'part.order'
+
+    def _default_request(self):
+        return self.env['technical_support.request'].browse(self._context.get('active_id'))
+
+    request_id = fields.Many2one('technical_support.request', default=_default_request, string='Request', track_visibility='onchange', readonly=True, states={'draft':[('readonly',False)]})
+
+    @api.onchange('request_id')
+    def onchange_request(self):
+        self.equipment_id = self.request_id.equipment_id
+        self.partner_id = self.request_id.client_id
+
+class PartLine(models.Model):
+    _inherit = 'part.line'
+
+    def _default_request(self):
+        return self.env['technical_support.request'].browse(self._context.get('active_id'))
+
+    request_id = fields.Many2one('technical_support.request', default=_default_request, string='Request', track_visibility='onchange')
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            move = self.env['part.order'].browse(vals['part_id'])
+            vals['request_id'] = move.request_id.id
+
+        lines = super(PartLine, self).create(vals_list)
+
+        return lines
