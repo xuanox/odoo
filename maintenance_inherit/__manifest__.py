80d27fb29e6ca1f34c31dee7f2e5ebd08555309e
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-08-12 13:32:30 -0500

    Update __manifest__.py

--- maintenance_inherit/__manifest__.py
+++ maintenance_inherit/__manifest__.py
@@ -7,7 +7,7 @@
     'category': 'Human Resources',
     'description': """
         Track equipments and maintenance requests""",
-    'depends': ['mail'],
+    'depends': ['maintenance'],
     'summary': 'Track equipment and manage maintenance requests',
     'website': 'https://www.odoo.com/page/tpm-maintenance-software',
     'data': [
