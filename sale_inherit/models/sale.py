5a6fa8d47860e4b033ccea77517f77e0fd9104c7
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-08-19 13:21:28 -0500

    update

--- sale_inherit/models/sale.py
+++ sale_inherit/models/sale.py
@@ -11,3 +11,11 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     commitment_date = fields.Datetime(required=True)
+
+    def _action_confirm(self):
+        """ Implementation of additionnal mecanism of Sales Order confirmation.
+            This method should be extended when the confirmation should generated
+            other documents. In this method, the SO are in 'sale' state (not yet 'done').
+        """
+
+        return True
