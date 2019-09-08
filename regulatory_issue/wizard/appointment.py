7967acfecea3550dc1e8a52710ea4c4222d0fdc6
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-09-07 22:56:45 -0500

    update

--- regulatory_issue/wizard/appointment.py
+++ regulatory_issue/wizard/appointment.py
@@ -19,8 +19,8 @@ class RegulatoryTechnicalFileAppointmentAssigned(models.TransientModel):
     ]
 
     date_planned = fields.Datetime('Planned Date', default=time.strftime('%Y-%m-%d %H:%M:%S'), required=True, track_visibility='onchange')
-    location_appointment = fields.Text('Appointment Location', required=True)
-    entity = fields.Selection(ENTITY_SELECTION, 'Entity', track_visibility='onchange')
+    entity_id = fields.Many2one('regulatory.entity', string='Entity', track_visibility='onchange')
+    location_homologation=fields.Text(related='entity_id.description', string='Homologation Location', readonly=True, track_visibility='onchange')
 
     def appointment_assigned(self):
         active_id = self._context.get('active_id')
@@ -28,6 +28,6 @@ class RegulatoryTechnicalFileAppointmentAssigned(models.TransientModel):
             request = self.env['regulatory.technical.file.registry'].browse(self._context.get('active_id'))
             request.write({'location_appointment':self.location_appointment})
             request.write({'date_planned':self.date_planned})
-            request.write({'entity':self.entity})
+            request.write({'entity_id':self.entity})
             request.action_appointment()
         return {'type': 'ir.actions.act_window_close',}
