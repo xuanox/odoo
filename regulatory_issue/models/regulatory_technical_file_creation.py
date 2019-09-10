4b4e1e29802dd484d2e4d807e48b7060629b531c
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-09-09 21:43:09 -0500

    update

--- regulatory_issue/models/regulatory_technical_file_creation.py
+++ regulatory_issue/models/regulatory_technical_file_creation.py
@@ -66,7 +66,7 @@ class RegulatoryTechnicalFileCreation(models.Model):
     location_homologation=fields.Text(related='entity_id.description', string='Homologation Location', readonly=True, track_visibility='onchange')
     tfr_ids = fields.One2many('regulatory.technical.file.registry', 'tfc_id', string='TFR')
     tfr_count = fields.Integer(compute='_tfr_count', string='TFR')
-    contact_id=fields.Many2one('res.partner', string='Factory Contact', track_visibility='onchange', required=True)
+    contact_ids = fields.Many2many('res.partner', 'regulatory_res_partner_rel', string='Contacts', states={'done': [('readonly', True)]})
     tag_ids = fields.Many2many('regulatory.tag', 'regulatory_tag_rel', 'tfm_id', 'tag_id', string='Tags', help="Classify and analyze your request like: Training, Service")
 
     def action_assigned(self):
