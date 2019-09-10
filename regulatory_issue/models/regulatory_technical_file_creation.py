f89c78427533ca1a0fba93811794b91f3463d7a4
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-09-09 22:13:38 -0500

    update

--- regulatory_issue/models/regulatory_technical_file_creation.py
+++ regulatory_issue/models/regulatory_technical_file_creation.py
@@ -67,7 +67,7 @@ class RegulatoryTechnicalFileCreation(models.Model):
     tfr_ids = fields.One2many('regulatory.technical.file.registry', 'tfc_id', string='TFR')
     tfr_count = fields.Integer(compute='_tfr_count', string='TFR')
     contact_ids = fields.Many2many('res.partner', 'regulatory_res_partner_rel', string='Contacts', states={'done': [('readonly', True)]})
-    tag_ids = fields.Many2many('regulatory.tag', 'regulatory_tag_rel', 'tfm_id', 'tag_id', string='Tags', help="Classify and analyze your request like: Training, Service")
+    tag_ids = fields.Many2many('regulatory.tag', 'regulatory_tag_rel', 'tfc_id', 'tag_id', string='Tags', help="Classify and analyze your request like: Training, Service")
 
     def action_assigned(self):
         self.write({'state': 'assigned'})
