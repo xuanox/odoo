bcf71e8a42a412e80dd4cafb1fc54e4a6017a07e
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-09-10 14:19:32 -0500

    up

--- regulatory_issue/models/regulatory_technical_criteria.py
+++ regulatory_issue/models/regulatory_technical_criteria.py
@@ -46,6 +46,7 @@ class RegulatoryTechnicalCriteria(models.Model):
     date_expiration_authenticated_seal = fields.Date(u'Date Expiration of the Authenticated Seal', track_visibility='onchange')
     description=fields.Text('Description', track_visibility='onchange')
     qty_available = fields.Integer('Quantity Available', default=0, help="Assign Quantity Available.", track_visibility='onchange')
+    minimum_quantity = fields.Integer('Minimum Quantity', default=0, help="Assign Minimum Quantity.", track_visibility='onchange')
     state = fields.Selection(STATE_SELECTION, 'Status', readonly=True, track_visibility='onchange',
         help="When the maintenance order is created the status is set to 'New'.\n\
         If the TC is Valid the status is set to 'Valid'.\n\
