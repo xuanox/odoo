aea8984f0547f31280abbd6041bfe18982251db4
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-09-15 02:46:45 -0500

    Update regulatory_technical_criteria.py

--- regulatory_issue/models/regulatory_technical_criteria.py
+++ regulatory_issue/models/regulatory_technical_criteria.py
@@ -108,6 +108,7 @@ class RegulatoryTechnicalCriteria(models.Model):
             self.change_expiration_tc_stamp()
             self._cron_change_state_tc_stamp()
         if self.is_stamp_to_expire == False or self.is_expired_stamp == True:
+            self.change_expiration_tc_stamp()
             self._cron_change_state_tc_stamp()
         if self.is_stamp_to_expire == True or self.is_expired_stamp == False:
             self._cron_change_state_tc_stamp()
