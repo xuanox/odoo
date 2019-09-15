2fb574d3fe5d8e4083820fbbe5e0128ae7aade70
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-09-15 03:07:56 -0500

    Update regulatory_technical_criteria.py

--- regulatory_issue/models/regulatory_technical_criteria.py
+++ regulatory_issue/models/regulatory_technical_criteria.py
@@ -104,13 +104,10 @@ class RegulatoryTechnicalCriteria(models.Model):
     def check_expiration_stamp(self):
         if self.is_stamp_to_expire == False or self.is_expired_stamp == False:
             self._cron_change_state_tc_stamp()
-        if self.is_stamp_to_expire == True or self.is_expired_stamp == True:
-            self.change_expiration_tc_stamp()
-            self._cron_change_state_tc_stamp()
         if self.is_stamp_to_expire == True:
             self.change_expiration_tc_stamp()
             self._cron_change_state_tc_stamp()
-        if self.is_expired_stamp == True:
+        if self.is_expired_stamp == False:
             self.change_expiration_tc_stamp()
             self._cron_change_state_tc_stamp()
         return super(RegulatoryTechnicalCriteria, self)
@@ -166,8 +163,8 @@ class RegulatoryTechnicalCriteria(models.Model):
         # set to expired if date is passed
         domain_expired_stamp = [('date_expiration_authenticated_seal', '<', today), ('is_expired_stamp', '=', False)]
         expired_tc_stamp = self.search(domain_expired_stamp)
-        expired_tc_stamp.set_stamp_to_expire_false()
         expired_tc_stamp.set_expired_stamp()
+        expired_tc_stamp.set_stamp_to_expire_false()
 
         return dict(expiration_stamp=tc_stamp_expired.ids, expired_stamp=expired_tc_stamp.ids)
 
