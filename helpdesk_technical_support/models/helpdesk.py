95502100ed1c21926f6f8388ab87ace63b4e40bf
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-09-30 15:06:07 -0500

    update

--- helpdesk_technical_support/models/helpdesk.py
+++ helpdesk_technical_support/models/helpdesk.py
@@ -47,7 +47,9 @@ class HelpdeskTicket(models.Model):
     detail_cause = fields.Text('Detail Causa', readonly=True)
     cause_reason = fields.Many2one('helpdesk.ticket.cause.reason', string='cause Reason', index=True, track_visibility='onchange')
     remote = fields.Boolean('Remote Attention', copy=False)
+    observation = fields.Boolean('Observation', copy=False)
     date_planned = fields.Datetime('Planned Date', default=time.strftime('%Y-%m-%d %H:%M:%S'), track_visibility='onchange')
+    date_entry = fields.Datetime('Planned Date', default=time.strftime('%Y-%m-%d %H:%M:%S'), track_visibility='onchange')
 
     technical_support_count = fields.Integer(compute='_technical_support_count', string='# Reports')
     duration = fields.Float('Real Duration', store=True)
@@ -194,6 +196,7 @@ class HelpdeskTicket(models.Model):
     @api.multi
     def end_all(self):
         return self.end_previous(doall=True)
+
 
 class HelpdeskTeam(models.Model):
     _inherit = 'helpdesk.team'
