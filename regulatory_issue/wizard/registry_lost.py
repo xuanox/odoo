d9614a6c45c83356efeee7542eb23e2876e1e19b
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-10-09 14:47:33 -0500

    update

--- regulatory_issue/wizard/registry_lost.py
+++ regulatory_issue/wizard/registry_lost.py
@@ -7,12 +7,14 @@ class RegulatoryTechnicalFileRegistryLost(models.TransientModel):
     _name = 'regulatory.technical.file.registry.lost'
     _description = 'Get Lost Reason'
 
-    lost_reason_id = fields.Many2one('regulatory.lost.reason', string='Lost Reason', required=True)
+    lost_reason_id = fields.Many2one('regulatory.technical.file.registry.lost.reason', string='Lost Reason', required=True)
+    reason_id = fields.Many2one('regulatory.lost.reason', required=True, string='Reason')
     description=fields.Text('Description')
 
     @api.multi
     def action_lost_reason_apply(self):
         registry = self.env['regulatory.technical.file.registry'].browse(self.env.context.get('active_ids'))
-        registry.write({'lost_reason_id': self.lost_reason_id.id})
+        registry.write({'lost_reason': self.lost_reason_id.id})
+        registry.write({'lost_reason_id': self.reason_id.id})
         registry.write({'description_lost':self.description})
         return registry.action_set_fails()
--- regulatory_issue/wizard/reject.py
+++ regulatory_issue/wizard/reject.py
@@ -13,6 +13,7 @@ class RegulatoryTechnicalFileRegistryReject(models.TransientModel):
     _name = 'regulatory.technical.file.registry.reject'
     _description = 'Registry Reject'
 
+    reject_reason_id = fields.Many2one('regulatory.technical.file.registry.reject.reason', required=True, string='Reject Reason')
     reason_id = fields.Many2one('regulatory.lost.reason', required=True, string='Reason')
     description=fields.Text('Description')
 
@@ -20,6 +21,7 @@ class RegulatoryTechnicalFileRegistryReject(models.TransientModel):
         active_id = self._context.get('active_id')
         if active_id:
             registry = self.env['regulatory.technical.file.registry'].browse(self._context.get('active_id'))
+            registry.write({'reject_reason':self.reject_reason_id.id})
             registry.write({'reject_reason_id':self.reason_id.id})
             registry.write({'description_reject':self.description})
             registry.action_rejected()
