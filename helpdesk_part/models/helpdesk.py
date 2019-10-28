bcc6160eefba73aa84636de719d17a88acf169c7
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-10-27 19:18:13 -0500

    update

--- helpdesk_part/models/helpdesk.py
+++ helpdesk_part/models/helpdesk.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 from odoo import api, fields, models, SUPERUSER_ID, _, exceptions
 import time
 import datetime as dt
@@ -11,6 +10,7 @@ from dateutil import relativedelta
 from datetime import datetime, timedelta
 from odoo.exceptions import UserError
 from dateutil.relativedelta import *
+
 
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
