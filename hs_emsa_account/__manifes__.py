455218f77a5a9cf3cce586d190a082c136ddb273
Luis <luis.dominguez19@yahoo.es>
2019-10-28 15:03:26 -0500

    module renamed

--- hs_emsa_account/__manifes__.py
+++ hs_emsa_account/__manifes__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-	'name': "hs_account_extra",
+	'name': "hs_emsa_account",
 
 	'summary': """
 		Configuraciones extra a accounting """,
@@ -9,28 +9,24 @@
 		Configuraciones personalizadas al modulo de contibilidad
 	""",
 
-	'author': "HS Consulting S.A.",
-	'website': "http://www.hconsul.com/odoo/",
+	'author': 'HS Consulting S.A.',
+	'website': 'http://www.hconsul.com/odoo/',
+	'maintainer': 'Luis Dominguez',
 
 	# Categories can be used to filter modules in modules listing
 	# Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
 	# for the full list
-	'category': 'other',
-	'version': '1',
+	'category': 'Invoicing & Payments',
+	'license': 'LGPL-3',
+	'version': '1.00',
 
 	# any module necessary for this one to work correctly
 	'depends': ['base', 'account'],
 
 	# always loaded
 	'data': [
-		# 'security/ir.model.access.csv',
 		'security/account_invoice_groups.xml'
 		'views/account_invoice_view.xml',
-	],
-
-	# only loaded in demonstration mode
-	'demo': [
-		#'demo/demo.xml',
 	],
 
 	'installable': True,
