c4235a08dfea3acc3bea14de1f945dac9fe9dcb1
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-12-02 16:00:27 -0500

    update

---
+++ helpdesk_technical_support/controllers/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_form.controllers.main import WebsiteForm
+
+
+class CustomWebsiteForm(WebsiteForm):
+
+    @http.route('''/helpdesk/<model("helpdesk.team", "[('use_website_helpdesk_form','=',True)]"):team>/submit''', type='http', auth="public", website=True)
+    def website_helpdesk_form(self, team, **kwargs):
+        res = super(CustomWebsiteForm, self).website_helpdesk_form()
+        equipments = request.env['equipment.equipment'].sudo().search([])
+        return res
