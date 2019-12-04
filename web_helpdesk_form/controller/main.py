dfa9ec2927fd72fe8aa7dfea5a2089ba990b9759
rocendotejadaemsa <r.tejada@electronicamedica.com>
2019-12-04 08:29:33 -0500

    update

--- web_helpdesk_form/controller/main.py
+++ web_helpdesk_form/controller/main.py
@@ -28,3 +28,54 @@ class WebsiteForm(WebsiteForm):
             if Partner:
                 request.params['partner_id'] = Partner.id
         return super(WebsiteForm, self).website_form(model_name, **kwargs)
+
+
+    @http.route('''/helpdesk/<model("helpdesk.team", "[('use_website_helpdesk_form','=',True)]"):team>/submit''', type='http', auth="public", website=True)
+    def web_helpdesk_form(self, team, **kwargs):
+        if not team.active or not team.website_published:
+            return request.render("website_helpdesk.not_published_any_team")
+        default_values = {}
+        if request.env.user.partner_id != request.env.ref('base.public_partner'):
+            default_values['name'] = request.env.user.partner_id.name
+            default_values['email'] = request.env.user.partner_id.email
+
+        equipments = request.env['equipment.equipment'].sudo().search([])
+        values = {'equipments' : equipments}
+        return request.render("web_helpdesk_form.ticket_submit", {'team': team, 'default_values': default_values}, values)
+
+
+    @http.route(['/intervention/request'], type='http', auth='public', website=True)
+    def register(self, redirect=None, **post):
+        equipments = request.env['equipment.equipment'].sudo().search([])
+        values = {'equipments' : equipments}
+        return request.render("web_helpdesk_form.request", values)
+
+
+    def _process_registration(self, post):
+        request.env['helpdesk'].sudo().create({
+            'category_id' : post.get('category_id'),
+            'equipment_id': post.get('equipment_id'),
+            'motif': post.get('motif'),
+            'priority': post.get('priority'),
+            'failure_type': post.get('failure_type'),
+            'partner': post.get('partner_id'),
+            'zone_id': post.get('zone_id'),
+            'state_machine': post.get('state_id'),
+
+    })
+
+
+    def _form_validate(self, data):
+        error = dict()
+        error_message = []
+
+        # Validation
+        for field_name in self.MANDATORY_BILLING_FIELDS:
+            if not data.get(field_name):
+                error[field_name] = 'missing'
+
+        # error message for empty required fields
+        if [err for err in error.values() if err == 'missing']:
+            error_message.append(_('Some required fields are empty.'))
+
+        return error, error_message
