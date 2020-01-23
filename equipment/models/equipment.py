f392bb9ffb38cf085e8aa95a90968809e7ade075
rocendotejadaemsa <r.tejada@electronicamedica.com>
2020-01-23 11:46:13 -0500

    Update equipment.py

--- equipment/models/equipment.py
+++ equipment/models/equipment.py
@@ -76,7 +76,7 @@ class equipment_equipment(models.Model):
     _name = 'equipment.equipment'
     _description = 'Equipment'
     _inherit =  ['mail.thread', 'mail.activity.mixin']
-    _order = "modality_id, name"
+    _order = "modality_id"
 
     def _read_group_state_ids(self, domain, read_group_order=None, access_rights_uid=None, team='3'):
         access_rights_uid = access_rights_uid or self.uid
