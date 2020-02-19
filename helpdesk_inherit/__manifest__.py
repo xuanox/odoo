e4e0c0a9f42a545e0e2a9291f420bc0f123d8853
rocendotejadaemsa <r.tejada@electronicamedica.com>
2020-02-19 12:35:21 -0500

    update helpdesk mail data

---
+++ helpdesk_inherit/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Helpdesk Inherit',
+    'version': '1.0',
+    "author": "Rocendo Tejada - Electronica Médica",
+    'category': 'Hidden',
+    'description': '',
+    'depends': ['helpdesk'],
+    'data': [
+        'data/mail_data.xml',
+    ],
+    'auto_install': True,
+}
