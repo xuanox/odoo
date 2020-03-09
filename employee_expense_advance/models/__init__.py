33ba48f5b2be4c2ed24890527f0d4994fc3a0e32
rocendotejadaemsa <r.tejada@electronicamedica.com>
2020-03-09 13:22:35 -0500

    add employee expense

---
+++ employee_expense_advance/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import employee_advance_expense
+from . import advance_expense_line
+from . import hr_expense
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
